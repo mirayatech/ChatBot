6c4932324b0dfe331bc2a4ef5128095a1d5c7b89
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-05-24 21:02:16 +0200

    function - get first message

--- script/chat.js
+++ script/chat.js
@@ -32,3 +32,17 @@ function getTime() {
   let time = hours + ":" + minutes;
   return time;
 }
+
+// Gets the first message
+function firstBotMessage() {
+  let firstMessage = "How's it going?";
+  document.getElementById("botStarterMessage").innerHTML =
+    '<p class="botText"><span>' + firstMessage + "</span></p>";
+
+  let time = getTime();
+
+  $("#chat-timestamp").append(time);
+  document.getElementById("userInput").scrollIntoView(false);
+}
+
+firstBotMessage();
