8ca4061784865d87611f88714f2f24a9eac50553
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-05-24 21:01:31 +0200

    function - get time

--- script/chat.js
+++ script/chat.js
@@ -14,3 +14,21 @@ for (let i = 0; i < coll.length; i++) {
     }
   });
 }
+
+// Get Time
+function getTime() {
+  let today = new Date();
+  hours = today.getHours();
+  minutes = today.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  let time = hours + ":" + minutes;
+  return time;
+}
